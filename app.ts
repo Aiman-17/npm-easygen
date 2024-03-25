6271b4f3ecce21376b06eeb95de64ba20a880eaf
Aiman-17 <aimenf307@gmail.com>
2024-03-25 04:47:08 -0700

    guess a random number

---
+++ app.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNum = Math.floor (Math.random() * 7 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNum",
+        type: "number",
+        message: "please guess a random number from 1-7;",
+    }
+  
+]);
+
+if (answer.randomNum === randomNum) {
+    console.log("Congratulations! You won");
+
+} else {
+    console.log ("You lose🩻");
+}
+
+
